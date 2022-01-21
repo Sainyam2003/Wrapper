b5e0cbff02b67d980c36746572a836184e2a9091
Sainyam Chandra <42771888+Sainyam2003@users.noreply.github.com>
2022-01-21 15:00:25 +0100

    wrapper

---
+++ Wrapper/Program.cs
@@ -0,0 +1,63 @@
+﻿//
+// Wrapper: breaks or wraps a string and words
+//
+namespace Wrapper
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Wrapper.
+    /// </summary>
+    public class Wrapper
+    {
+        /// <summary>
+        /// Error message.
+        /// </summary>
+        private static readonly string error = "Invalid input!";
+
+        /// <summary>
+        /// Main function.
+        /// </summary>
+        public static void Main()
+        {
+            Console.WriteLine("Enter a word/sentence:");
+            var word = Console.ReadLine();
+
+            if (word == null || word.GetType() != typeof(string))
+            {
+                Console.WriteLine(error);
+                return;
+            }
+
+            Console.WriteLine("Enter a integar:");
+            var number = Console.ReadLine();
+
+            if (number == null || int.TryParse(number, out int columnNumber) == false || columnNumber <= 0)
+            {
+                Console.WriteLine(error);
+                return;
+            }
+
+            Console.Write(string.Join(Environment.NewLine, Wrap(word, columnNumber)));
+        }
+
+        /// <summary>
+        /// Wraps/breaks the string and words based on column size
+        /// </summary>
+        /// <returns>A string.</returns>
+        public static IEnumerable<string> Wrap(string word, int columnNumber)
+        {            
+            var result=new List<string>();
+            var words = word.Split(' ');
+            foreach (var w in words)
+            {
+                for (var i = 0; i < w.Length; i += columnNumber)
+                {
+                    result.Add(w.Substring(i, Math.Min(columnNumber, w.Length - i)));
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ WrapperTests/ProgramTests.cs
@@ -0,0 +1,59 @@
+﻿//
+// WrapperTests
+//
+namespace Wrapper.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Linq;
+
+    /// <summary>
+    /// The program tests.
+    /// </summary>
+    [TestClass()]
+    public class WrapperTests
+    {
+        /// <summary>
+        /// Does the not break words.
+        /// </summary>
+        [TestMethod()]
+        public void ColumnsNumberGreaterThanWordLength()
+        {
+            var result = Wrapper.Wrap("repeat repeat", 8);            
+            string[] baseline= { "repeat", "repeat" };
+            Assert.IsTrue(Enumerable.SequenceEqual(baseline, result));
+        }
+
+        /// <summary>
+        /// Breaks the words.
+        /// </summary>
+        [TestMethod()]
+        public void ColumnsNumberLowerThanWordLength()
+        {
+            var result = Wrapper.Wrap("repeat repeat", 3);
+            string[] baseline = { "rep", "eat", "rep", "eat" };
+            Assert.IsTrue(Enumerable.SequenceEqual(baseline, result));
+        }
+
+        /// <summary>
+        /// Columns the number lower than words in sentence length.
+        /// </summary>
+        [TestMethod()]
+        public void ColumnsNumberLowerThanWordsInSentenceLength()
+        {
+            var result = Wrapper.Wrap("This is a test string.", 2);
+            string[] baseline = { "Th", "is", "is", "a", "te", "st", "st", "ri", "ng", "." };
+            Assert.IsTrue(Enumerable.SequenceEqual(baseline, result));
+        }
+
+        /// <summary>
+        /// Columns the number greater than words in sentence length.
+        /// </summary>
+        [TestMethod()]
+        public void ColumnsNumberGreaterThanWordsInSentenceLength()
+        {
+            var result = Wrapper.Wrap("This is a test string.", 7);
+            string[] baseline = { "This", "is", "a", "test", "string."};
+            Assert.IsTrue(Enumerable.SequenceEqual(baseline, result));
+        }
+    }
+}
